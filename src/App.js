ea4e9e0ab7097e8d9c9ec5f6708eb17826a854b5
meenah200 <128811963+meenah200@users.noreply.github.com>
2023-11-15 02:34:03 -0600

    Added my Github repository

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ export default function App() {
     <div className="App">
       <h1>
         Weather App
-      </h1>     
+      </h1>   
+      <footer>
+        This project was coded by odenike Aminat and <a href="https://github.com/meenah200/react-weather-app">Open-sourced</a> on Github
+      </footer>  
     </div>
   );
 }
