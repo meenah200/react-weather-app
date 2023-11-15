d4a42ef1f232be95d7a755c088cb63201c0e9ae8
meenah200 <128811963+meenah200@users.noreply.github.com>
2023-11-15 02:57:21 -0600

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from 'react';
 
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>
         Weather App
       </h1>   
       <footer>
         This project was coded by odenike Aminat and <a href="https://github.com/meenah200/react-weather-app">Open-sourced</a> on Github
-      </footer>  
+      </footer> 
+      </div> 
     </div>
   );
 }
