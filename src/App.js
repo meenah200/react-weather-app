76e284a2e446688e1a483ea22ed58ac37c46d625
meenah200 <128811963+meenah200@users.noreply.github.com>
2023-11-15 16:49:22 -0600

    Added the API integration

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
+      <Weather defaultCity="Victoria Island"/>
       <footer>
         This project was coded by odenike Aminat and <a href="https://github.com/meenah200/react-weather-app">Open-sourced</a> on Github
       </footer> 
